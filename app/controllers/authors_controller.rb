3e42bf97cba6ec9e4c3ea1b6928fcd46e324773d
Sofia <jonssonsofia93@gmail.com>
2019-04-03 10:30:23 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,14 +4,19 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @post=Post.new()
   end
+
+
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
-
   private
 
   def author_params
